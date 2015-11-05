97edcaa34e4066ad4db48623887da72e27e2b9f2
Neeraj Nabar <neerajnabar@gmail.com>
2015-11-04 21:57:48 -0800

    added some more functions to arraydemos.

--- src/arrays/arraydemos.c
+++ src/arrays/arraydemos.c
@@ -8,13 +8,66 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void displayArraySize(int a[])
-{
+void displayArraySize(int a[]) {
 	int* ptr = a;
 	int size = 0;
-	while(*ptr != NULL) {
+	while (*ptr != NULL) {
 		size++;
 		ptr++;
 	}
-	printf("Size of the array is %d\n",size);
+	printf("Size of the array is %d\n", size);
+}
+
+void merge() {
+	int a[3] = { 1, 2, 3 };
+	int b[4] = { 3, 4, 5, 6 };
+
+	int l1 = 3, l2 = 4;
+
+	int sorted_array[l1 + l2];
+	int* sortedArrayPtr = sorted_array;
+	int *arrayOfPtrs[2] = { a, b };
+
+	int tl = l1 + l2;
+	int i;
+	for (i = 0; i < tl; i++) {
+		int j = 0, k = 0;
+		if (j < l1 && k < l2) {
+			if (*arrayOfPtrs[0] < *arrayOfPtrs[1]) {
+				*sortedArrayPtr = *arrayOfPtrs[0];
+				sortedArrayPtr++;
+				arrayOfPtrs[0]++;
+			} else if (*arrayOfPtrs[0] > *arrayOfPtrs[1]) {
+				*sortedArrayPtr = *arrayOfPtrs[1];
+				sortedArrayPtr++;
+				arrayOfPtrs[1]++;
+			} else if (*arrayOfPtrs[0] == *arrayOfPtrs[1]) {
+				*sortedArrayPtr = *arrayOfPtrs[1];
+				sortedArrayPtr++;
+				arrayOfPtrs[0]++;
+				arrayOfPtrs[1]++;
+			}
+			j++;
+			k++;
+		}
+		if (j == l1 && k < l2) {
+			*sortedArrayPtr = *arrayOfPtrs[1];
+			sortedArrayPtr++;
+			arrayOfPtrs[1]++;
+			k++;
+		}
+		if (k == l2 && j < l1) {
+			*sortedArrayPtr = *arrayOfPtrs[0];
+			sortedArrayPtr++;
+			arrayOfPtrs[0]++;
+			j++;
+		}
+	}
+	*(sortedArrayPtr + 1) = NULL;
+	int p = 0;
+	sortedArrayPtr = sorted_array;
+	while (sorted_array[p] != NULL) {
+		printf("%d ", sorted_array[p]);
+		p++;
+	}
 }
--- src/main.c
+++ src/main.c
@@ -10,18 +10,20 @@
 #include "basics/sizesof.h"
 
 int main() {
-	pListNode n1, n2, n3;
-	pListNode head;
-	n1 = createNode(1);
-	n2 = createNode(2);
-	n3 = createNode(3);
-
-	head = n1;
-	n1->next = n2;
-	n2->next = n3;
-	n3->next = NULL;
-
-	traverseList(head);
+//	pListNode n1, n2, n3;
+//	pListNode head;
+//	n1 = createNode(1);
+//	n2 = createNode(2);
+//	n3 = createNode(3);
+//
+//	head = n1;
+//	n1->next = n2;
+//	n2->next = n3;
+//	n3->next = NULL;
+//
+//	traverseList(head);
+//	merge();
+	arrptrdemo();
 
 	return 0;
 }
